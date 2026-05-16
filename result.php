f5df675d0aee43a22d6b2141420f88e9b3e1c99a
TechNikh <info@technikh.com>
2026-05-16 13:28:32 +0530

    Add redirect page to result.html

---
+++ result.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Redirect</title>
+
+  <meta http-equiv="refresh" content="0; url=result.html">
+
+  <script>
+    window.location.replace("result.html");
+  </script>
+</head>
+<body>
+  <a href="result.html">Continue</a>
+</body>
+</html>
